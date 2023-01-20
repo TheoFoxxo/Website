d150c7d243ce310411589c85af4641bd21d9c11c
HackDev <113368165+TheodoreTheFox@users.noreply.github.com>
2023-01-20 18:45:38 +0000

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
-import '@/styles/globals.scss'
-import '@/styles/Home.scss'
+import '../styles/globals.scss'
+import '../styles/Home.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />

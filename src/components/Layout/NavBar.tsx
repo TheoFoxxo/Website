1adede9aeacbef8af6fc97d649683712b146f362
HackDev <113368165+TheodoreTheFox@users.noreply.github.com>
2023-01-20 18:28:33 +0000

    Patch?

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -1,5 +1,6 @@
 import styles from '@/styles/NavBar.module.scss'
 import { Config } from '@/utils/config'
+import Link from 'next/link'
 
 export const NavBar = () => {
     return (
@@ -7,10 +8,10 @@ export const NavBar = () => {
             <nav className={styles.nav}>
                 <h2><a className={styles.logo} href="/">Theodore</a></h2>
                 <ul>
-                    <li><a href="/">Home</a></li>
+                    <li><Link href="/">Home</Link></li>
                     <li><a onClick={() => alert("Lazy FUICK")}>Blogs</a></li>
-                    <li><a href="/cuties">Cuties</a></li>
-                    <li><a href="/about ">About Me</a></li>
+                    <li><Link href="/cuties">Cuties</Link></li>
+                    <li><Link href="/about ">About Me</Link></li>
                 </ul>
                 <button className={styles.button} type="button"> <a href={Config.email} >Contact</a></button>
             </nav>

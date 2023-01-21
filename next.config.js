898b97dd10621c1a2c1a7d7337b45f1374767016
HackDev <113368165+TheodoreTheFox@users.noreply.github.com>
2023-01-21 18:07:29 +0000

    Patch?

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 }
 
 module.exports = nextConfig
+
+// module.exports = {
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) {
+//       config.resolve.fallback.fs = false;
+//     }
+//     return config;
+//   },
+// }

ff0b4cd5a6b9bf7a2c2383e265210baacec5eca4
HackDev <113368165+TheodoreTheFox@users.noreply.github.com>
2023-02-01 15:03:28 +0000

    Patch

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,5 @@
-import React from "react";                                   
+import React from "react";   
+import { BrowserRouter } from 'react-router-dom';                                
 import { NavBar } from "./NavBar";
 import { Config } from '../../utils/config'
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     <>
       <div>
       <TheoBox/>
-      <NavBar />
       </div>
     </>
   )

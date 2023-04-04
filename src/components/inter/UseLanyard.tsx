93b21055979d97708422865099869696f6ae2dd2
HackDev <80620538+Hackx2@users.noreply.github.com>
2023-04-04 01:54:31 +0100

    1

--- src/components/inter/UseLanyard.tsx
+++ src/components/inter/UseLanyard.tsx
@@ -1,17 +1,18 @@
-import { Config } from '@/utils/config';
 import { useLanyardWS } from 'use-lanyard';// such a cool package
+import Link from 'next/link'
+interface Iprops {
+    discordid: any
+}
 
-export default function UseLanyard() {
-    const DISCORD_ID = "664017668629856266";
-    const data = useLanyardWS(DISCORD_ID);
+export default function UseLanyard({ discordid }: Iprops) {
+  //  const DISCORD_ID = "664017668629856266";
+    const data = useLanyardWS(discordid);
 
     let act = null;
     let icon;
     let pain = "";
     let dot;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     try {
         act = `${data?.activities[0].state}`
     } catch(e) {
@@ -19,11 +20,7 @@ export default function UseLanyard() {
         act = "I'm not doing anything."
         console.log(e)
     }
-=======
->>>>>>> parent of 1a880d4 (i forgor)
 
-=======
->>>>>>> parent of 4b6b3a8 (I'm gonna make everything a customization...for custmisation)
     try {
         if (!data?.activities[0].emoji?.animated === true) {
             pain = `https://cdn.discordapp.com/emojis/${data?.activities[0].emoji?.id}.png?size=28&quality=lossless`
@@ -37,15 +34,8 @@ export default function UseLanyard() {
     switch (data?.discord_status) {
         case "offline":
             icon = `rounded absolute w-3 h-3 bg-gray-400`
-<<<<<<< HEAD
             act = "I'm Asleep"
-<<<<<<< HEAD
             pain = `https://cdn.discordapp.com/emojis/854817372816015392.webp?size=28&quality=lossless`
-=======
-            act = "I'm Asleep 💤"
->>>>>>> parent of 4b6b3a8 (I'm gonna make everything a customization...for custmisation)
-=======
->>>>>>> parent of 1a880d4 (i forgor)
             break;
         case "dnd":
             icon = `absolute w-3 h-3 bg-red-400 rounded`
@@ -57,22 +47,6 @@ export default function UseLanyard() {
             icon = `absolute w-3 h-3 bg-yellow-400 rounded`
             break;
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    try {
-        act = `${data?.activities[0].state}`
-    } catch {
-        pain = ``
-    }
->>>>>>> parent of 4b6b3a8 (I'm gonna make everything a customization...for custmisation)
-=======
-    try {
-        act = `${data?.activities[0].state}`
-    } catch(e) {
-        pain = `https://cdn.discordapp.com/emojis/854817372816015392.webp?size=28&quality=lossless`
-    }
->>>>>>> parent of 1a880d4 (i forgor)
 
     if (!data?.listening_to_spotify){
         dot =" • ";
@@ -85,7 +59,7 @@ export default function UseLanyard() {
     return (
         <div className='grid place-items-center'>
         <div className="absolute rounded-md p-4 w-[20em] text-center border-gray bg-[#282828] border-dotted border flex">
-            <img src={`https://cdn.discordapp.com/avatars/${DISCORD_ID}/${data?.discord_user.avatar}.webp`} className="rounded-lg w-[100px] border-gray border-dotted border" />
+            <img src={`https://cdn.discordapp.com/avatars/${discordid}/${data?.discord_user.avatar}.webp`} className="rounded-lg w-[100px] border-gray border-dotted border" />
             <div className="my-auto ml-3">
                 <p className="text-lg flex">
                     {data?.discord_user.username}{"#"}{data?.discord_user.discriminator}
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -18,7 +18,7 @@ export default function About() {
                 <p className={styless.p}><strong>About</strong></p>
                 <h2 className={styless.owos}></h2>
                 <Space />
-                <UseLanyard />
+                <UseLanyard discordid={"664017668629856266"}/>
                 <Space />
                 <div className={styles.hmm}>
                     <article className={styles.article}>

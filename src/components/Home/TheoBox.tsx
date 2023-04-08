8e221fa737de2af882a8a0f58bb13194305c54ed
HackDev <80620538+Hackx2@users.noreply.github.com>
2023-04-08 22:17:11 +0100

    ok

--- src/components/Home/TheoBox.tsx
+++ src/components/Home/TheoBox.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 import styles from '../../styles/hero.module.scss'
 
 import { oldConfig, Config } from '../../utils/config'
-import { Buttons } from './Buttons'
+import Buttons from './Buttons'
 
+//** This is fucking deprecated */
 export const TheoBox = () => {
     const ihatevercel = `Hi, I'm ${Config.name}.`
     return (
@@ -13,7 +14,7 @@ export const TheoBox = () => {
             <h5>{oldConfig.hobby}</h5>
             <p>{Config.description}</p>
             <ul>
-                <Buttons/>
+                <Buttons linkFuck={undefined} faIcon={undefined}/>
             </ul>
         </div>
     )
